237ff8270156db12e13a211005b8ea93986dd241
Pedro Monteiro <pedroafmonteiro10@gmail.com>
2024-02-29 20:17:46 +0000

    exercise 2 - week 3

--- Week 3 (February 26)/P03/Interval/interval.cpp
+++ Week 3 (February 26)/P03/Interval/interval.cpp
@@ -2,6 +2,31 @@
 #include "interval.h"
 using namespace std;
 
+bool earlier_than(time_of_day a, time_of_day b) {
+    if (a.h == b.h) {
+        if (a.m < b.m) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (a.h < b.h) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 interval intersection(interval a, interval b) {
-    
+    interval r;
+    if (earlier_than(a.start, b.end) && earlier_than(b.start, a.end)) {
+        r.start = earlier_than(a.start, b.start) ? b.start : a.start;
+        r.end = earlier_than(a.end, b.end) ? a.end : b.end;
+    } else {
+        r = { { 0 , 0 } , { 0 , 0 } };
+    }
+    return r;
+}
+
+int main() {
+    return 0;
 }
