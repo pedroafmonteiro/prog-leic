55696df6dbefe8139bd85751d7f08e61c9b2a14a
Pedro Monteiro <pedroafmonteiro10@gmail.com>
2024-03-09 20:35:49 +0000

    Refactor hstr_to_integer function to handle uppercase hexadecimal characters

--- P04/HSTR to integer/hstr-to-integer.cpp
+++ P04/HSTR to integer/hstr-to-integer.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 unsigned long hstr_to_integer(const char hstr[]) {
     int i = 0;
-    unsigned long sum = 0;
+    unsigned long sum = 0, val;
     while (hstr[i] != '\0') {
-        if (hstr[i] >= '0' && hstr[i] <= '9') {
-            unsigned long val = int(hstr) - 48;
-            sum = sum*16 + val;
-        } else if (hstr[i] >= 'a' && hstr[i] <= 'z') {
-            unsigned long val = 
+        char c = hstr[i];
+        if (c >= '0' && c <= '9') {
+            val = c - '0';
+            sum = sum * 16 + val;
+        } else if ((c >= 'A' && c <= 'F')) {
+            val = c - 'A' + 10;
+            sum = sum * 16 + val;
+        } else if (c >= 'a' && c <= 'f') {
+            val = c - 'a' + 10;
+            sum = sum * 16 + val;
         }
         i++;
     }
@@ -17,6 +23,5 @@ unsigned long hstr_to_integer(const char hstr[]) {
 }
 
 int main() {
-    cout << hstr_to_integer("0");
     return 0;
 }
